4c436030ab4470916312bd526b5e8e1484df10c8
Stas Avdeev <d1g1texavd@gmail.com>
2021-01-15 12:25:00 +0300

    Выполнено задание "Singleton".

---
+++ 2.JavaCore/src/com/javarush/task/task14/Singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task14.Singleton;
+
+public class Singleton {
+
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    private Singleton() {
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ 2.JavaCore/src/com/javarush/task/task14/Singleton/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.Singleton;
+
+/* 
+Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
