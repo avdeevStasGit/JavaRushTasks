003158d1ac4c425497447fea624295698956f3d5
Stas Avdeev <d1g1texavd@gmail.com>
2021-02-08 22:23:08 +0300

    Выполнено задание "Ипподром(2)".

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,17 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hippodrome {
+
+    private List<Horse> horses = new ArrayList<Horse>();
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
 
     public static void main(String[] args) {
 
     }
-
 }
