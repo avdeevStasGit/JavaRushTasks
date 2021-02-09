45970fceb4bc263b0a1467ada8c8dd8b5b111297
Stas Avdeev <d1g1texavd@gmail.com>
2021-02-09 14:41:46 +0300

    Выполнено задание "Ипподром(5)".

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -2,9 +2,38 @@ package com.javarush.task.task21.task2113;
 
 public class Horse {
 
-    String name;
-    double speed;
-    double distance;
+    private String name;
+    private double speed;
+    private double distance;
 
+    public Horse(String name, double speed, double distance) {
 
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
 }
