e6a1a0861a7326646b45356d8e4ccefff243feb4
Stas Avdeev <d1g1texavd@gmail.com>
2021-02-20 15:33:25 +0300

    Выполнено задание "Транзакционность".

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -2,12 +2,11 @@ package com.javarush.task.task17.task1721;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-/* 
+/*
 Транзакционность
 */
 
