9d429bd599c2ffcb448093e68bd1b838fcd7b5f2
Stas Avdeev <d1g1texavd@gmail.com>
2021-02-21 14:14:47 +0300

    Выполнено задание "MVC3".

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,16 +1,24 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
+    //controller cannot contain business logic, only operate with views
 
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
     }
 
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
     }
 }
