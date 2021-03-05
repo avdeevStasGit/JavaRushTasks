0a5828f3d0a1c1cbbfd4477a8b1ba531800c87ad
Stas Avdeev <d1g1texavd@gmail.com>
2021-03-05 18:21:23 +0300

    Выполнено задание "Чат(2)".

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,39 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while(true) {
+            try {
+                String text = reader.readLine();
+
+                if(text != null) {
+                    return text;
+                }
+            } catch(IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+
+    }
+
+    public static int readInt() {
+        while(true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch(NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
 }
