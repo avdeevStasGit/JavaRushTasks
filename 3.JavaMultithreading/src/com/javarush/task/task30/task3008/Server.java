5419513ca2c320a65ad5520b5784ed9275c63e38
Stas Avdeev <d1g1texavd@gmail.com>
2021-03-09 13:32:45 +0300

    Выполнено задание "Чат(5)".

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,8 +1,36 @@
 package com.javarush.task.task30.task3008;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        int port = ConsoleHelper.readInt();
         
+        try(ServerSocket serverSocket = new ServerSocket(port))  {
+            ConsoleHelper.writeMessage("Чат сервер запущен");
+            while (true) {
+                // Ожидаем входящее соединение и запускаем отдельный поток при  его  принятии
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера");
+        }
+    }
+    
+    private static class Handler extends Thread {
+        private Socket socket;
+        
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            
+        }
     }
 }
