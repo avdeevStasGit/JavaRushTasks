b6c02863cd3189a457cfa7aef3fb68a8a0209b3d
Stas <d1g1texavd@gmail.com>
2021-11-06 18:35:21 +0300

    task03.task0303 update. Complete.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task03.task0303;
+/**
+ * Есть программа, которая принимает на вход возраст и определяет,
+ * нужно в школу или уже пора в институт. Но она работает неправильно.
+ * Например, пятилетнего ребенка отправляют в институт! Исправить программу несложно:
+ * достаточно в нужном месте поставить фигурные скобки.
+ */
+
+import java.util.Scanner;
+
+/* 
+Школа или институт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age < 18) {
+            if (age >= 6) {
+                System.out.println("нужно ходить в школу");
+            }
+        }
+        else {
+            System.out.println("пора в институт");
+        }
+    }
+}
