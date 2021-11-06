d21a91d77574cbd2b3a749f1e878b47f7e66c045
Stas <d1g1texavd@gmail.com>
2021-11-06 18:58:48 +0300

    task03.task0302 update. Complete

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -1,5 +1,13 @@
 package com.javarush.task.pro.task03.task0302;
 
+/**
+ * Ввести с клавиатуры имя и возраст. Если возраст в пределах 18-28 (включительно),
+ * то вывести надпись "Имя, явитесь в военкомат", где Имя - это имя, введенное ранее с клавиатуры.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -7,8 +15,15 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String militaryCommissar = ", явитесь в военкомат";
-        //напишите тут ваш код
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        if(age >= 18 && age <= 28) {
+                System.out.println(name + militaryCommissar);
+        }
     }
 }
