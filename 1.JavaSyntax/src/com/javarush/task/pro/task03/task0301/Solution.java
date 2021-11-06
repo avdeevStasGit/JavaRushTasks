452dbc38ed0039a729027b5a45340764a753ac3a
Stas <d1g1texavd@gmail.com>
2021-11-06 18:28:29 +0300

    task03.task0301 update, complete.

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.pro.task03.task0301;
  иначе - вывести надпись "на улице тепло".
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -11,9 +14,20 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String cold = "на улице холодно";
         String warm = "на улице тепло";
-        //напишите тут ваш код
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+        int temp = Integer.parseInt(text);
+
+        if(temp < 0) {
+            System.out.println(cold);
+        }
+        else if(temp >= 0) {
+            System.out.println(warm);
+        }
+
     }
 }
