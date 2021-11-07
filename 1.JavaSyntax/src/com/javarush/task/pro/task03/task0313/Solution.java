cd7b29de17c8446e4994ea029aa5c9d9a0b0c109
Stas <d1g1texavd@gmail.com>
2021-11-07 19:05:26 +0300

    task03.task0313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task03.task0313;
+
+/**
+ * В методе main напиши код попарного сравнения по ссылке строк string1, string2 и string3
+ * с выводом соответствующего сообщения после каждого сравнения:
+ * "ссылки на строки одинаковые" или "ссылки на строки разные".
+ *
+ * Порядок сравнения должен быть следующим:
+ * - строку string1 со строкой string2
+ * - строку string1 со строкой string3
+ * - строку string2 со строкой string3.
+ */
+
+public class Solution {
+    public static String string1 = "Амиго";
+    public static String string2 = string1;
+    public static String string3 = new String(string1);
+
+    public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+        //напишите тут ваш код
+        System.out.println(same);
+        System.out.println(different);
+    }
+}
