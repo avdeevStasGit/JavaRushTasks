3762d36a5fdfe60a652daf3061db0e12483f7fd4
Stas <d1g1texavd@gmail.com>
2021-11-07 18:54:04 +0300

    task03.task0309 complete

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
@@ -17,10 +17,7 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int numberA = scanner.nextInt();
         int numberB = scanner.nextInt();
-        if (numberA > numberB) {
-            System.out.println(numberA);
-        } else {
-            System.out.println(numberB);
-        }
+
+        System.out.println(numberA > numberB ? numberA : numberB);
     }
 }
