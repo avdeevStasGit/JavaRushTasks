8a631daa327fb6b066a2a945394d89a3729cd802
Stas <d1g1texavd@gmail.com>
2021-11-10 18:44:04 +0300

    task04.task0403 complete

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -1,5 +1,13 @@
 package com.javarush.task.pro.task04.task0403;
 
+/**
+ * Напишем программу, в которой нужно вводить с клавиатуры целые числа и считать их сумму, пока пользователь не введет слово "ENTER".
+ * Вывести на экран полученную сумму и завершить программу.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -8,7 +16,21 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        boolean isExit = false;
+        while (!isExit) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum = sum + number;
+            } else if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("ENTER")) {
+                    isExit = true;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
